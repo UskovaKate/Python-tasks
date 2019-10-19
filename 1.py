6b7c25edf03604e3d29cdac5fe6a582a5486f92c
UskovaKate <55867937+UskovaKate@users.noreply.github.com>
2019-10-19 08:12:29 +0400

    1 задание

---
+++ 1.py
@@ -0,0 +1,9 @@
+Python 3.6.5 |Anaconda, Inc.| (default, Mar 29 2018, 13:32:41) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> A="2019-10-19"
+>>> date=A.split("-")
+>>> print(date)
+['2019', '10', '19']
+>>> print(date[2]+"/"+date[1]+"/"+date[0])
+19/10/2019
+>>> 
