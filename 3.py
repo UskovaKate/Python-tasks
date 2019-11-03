1b17fe5622da8320511001b990e23332f4cbffdb
UskovaKate <55867937+UskovaKate@users.noreply.github.com>
2019-11-04 02:13:58 +0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+import math
+a=10.0
+b=5.0
+c=12.0
+P=a+b+c
+p=P/2.0
+S=math.sqrt(p*(p-a)*(p-b)*(p-c))
+print("Perimeter -",P)
+print("Area -",S)
---
+++ 4.py
@@ -0,0 +1,5 @@
+p=1567
+kg=float (p)/float(1000)
+t-float (p)/float(1000000)
+print(kg)
+print(t)
